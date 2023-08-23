daf5df66a96029cb321a702b0b528b2a336704bf
Magotgarang <m.guut@alustudent.com>
2023-08-23 11:36:21 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,6 @@
 
 exec { 'ULIMIT':
   onlyif   => 'test -e /etc/default/nginx',
-  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  command  => 'sed -i "5S/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 } 
