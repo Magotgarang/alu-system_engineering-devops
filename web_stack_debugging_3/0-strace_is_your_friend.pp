5476cbf5f7afa343e0a4e09d1b9f400deed5fb86
Magotgarang <m.guut@alustudent.com>
2023-08-23 10:36:22 +0000

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fix appache 500, automate with puppet
+# Fix Appache 500, automate with puppet
 exec { 'fixing typo..':
 onlyif => 'test -e /var/www/html/wp-settings.php',
 command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-path  => '/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin',
+path     => '/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin',
 }
